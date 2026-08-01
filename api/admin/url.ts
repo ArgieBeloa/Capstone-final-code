f70ff66db9f16a9cd9bc048ce3fe1e96c2334021
argie <argiepbeloa@gmail.com>
2026-08-01 09:17:47 +0800

    fix url local secure storage token

--- api/admin/url.ts
+++ api/admin/url.ts
@@ -1,6 +1,6 @@
 // offline
 // export const GLOBAL_URL = "http://localhost:8080";
-export const GLOBAL_URL = "http://192.168.1.4:8080";
+// export const GLOBAL_URL = "http://192.168.1.4:8080";
 
 // online
-// export const GLOBAL_URL = "https://securebackend-ox2e.onrender.com";
+export const GLOBAL_URL = "https://securebackend-ox2e.onrender.com";
